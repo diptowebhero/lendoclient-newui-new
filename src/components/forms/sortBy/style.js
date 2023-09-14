689fe63168c69abc65add5bd38310abc89217db4
ferdousalam007 <iamferdousalam007@gmail.com>
2023-09-14 19:07:26 +0600

    select add icon

--- src/components/forms/sortBy/style.js
+++ src/components/forms/sortBy/style.js
@@ -6,16 +6,22 @@ export const Style = styled.div`
     
     .ant-select-selector{
         background-color: #1A1C1E;
+        
+
     }
     .ant-select:not(.ant-select-customize-input) .ant-select-selector  {
         position: relative;
         background-color:#1A1C1E;
         border: none;
         border-radius: 10px;
+        padding-left:37px;
         transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
     }
     .ant-select-item {
         background-color:#1A1C1E;
+    }
+    .place-hol{
+        // padding-left:25px;
     }
     .rc-virtual-list-holder{
         background-color:#1A1C1E;
